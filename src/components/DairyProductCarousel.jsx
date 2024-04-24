00f07f7f134cafbe4cf049d8edbf846cae86e470
SawantAchal <achals1718@gmail.com>
2024-04-25 03:01:53 +0530

    created  carousel

--- src/components/DairyProductCarousel.jsx
+++ src/components/DairyProductCarousel.jsx
@@ -1,16 +1,45 @@
 import React from 'react'
 import Card from './Card'
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 const DairyProductCarousel = ({data}) => {
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 5,
+      slidesToSlide: 3, // Number of slides to slide when arrow clicked
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 3,
+      slidesToSlide: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1.5,
+      slidesToSlide: 1,
+    },
+  };
+  
   return (
     <div>DairyProductCarousel
-              {
+            <Carousel
+      responsive={responsive}
+      autoPlay={true}
+      autoPlaySpeed={2000}
+      infinite={true}
+      arrows={true}
+      >
+                      {
             data.map((item , i) => (
                 <div key={i}>
                     <Card image={item.image} Name={item.Name} BrandName={item.BrandName} Type={item.Type} Price={item.Price} MRP={item.MRP}  Avb={item.Avb} Unit={item.Unit} Qty={item.Qty}/>
                 </div>
             ))
         }
+      </Carousel>
+
     </div>
   )
 }
