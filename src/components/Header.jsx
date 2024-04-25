7fb16783752dce92f62050c5c26e3ddfb514abc3
SawantAchal <achals1718@gmail.com>
2024-04-25 20:10:55 +0530

    add horizontall scroll bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { TbDeviceMobileCharging } from "react-icons/tb";
 import { GrCreditCard } from "react-icons/gr";
 import { FaHandHoldingUsd } from "react-icons/fa";
@@ -9,34 +9,36 @@ import { FaFilePen } from "react-icons/fa6";
 const Header = () => {
   return (
     <>
-        <header className='relative flex justify-around bg-cyan-950 text-white text-center pt-20 z-0 p-6 w-full '>
-            <section>
-                <TbDeviceMobileCharging  className='h-16 w-16 text-sm p-3 rounded-full bg-white text-cyan-950'/>
-                <p>RECHARGE</p>
-            </section>
-            <section>
-                <GrCreditCard className='h-16 w-16 text-sm p-3 rounded-full bg-white text-cyan-950'/>
-                <p>BILL PAYMENT </p>
-            </section>
-            <section>
-                <FaHandHoldingUsd className='h-16 w-16 text-sm p-3 rounded-full bg-white text-cyan-950'/>
-                <p>LOAN</p>
-            </section>
-            <section>
-                <IoNewspaperOutline className='h-16 w-16 text-sm p-3 rounded-full bg-white text-cyan-950'/>
-                <p>NEWS</p>
-            </section>
-            <section>
-                <FaPeopleGroup className='h-16 w-16 text-sm p-3 rounded-full bg-white text-cyan-950'/>
-                <p>JOBS</p>
-            </section>
-            <section>
-                <FaFilePen className='h-16 w-16 text-sm p-3 rounded-full bg-white text-cyan-950'/>
-                <p>BLOGS</p>
-            </section>
-        </header>
+      <header className='relative bg-cyan-950 text-white text-center pt-20 z-0 p-6 w-full overflow-x-auto whitespace-no-wrap'>
+        <div className="flex justify-between">
+          <section className="flex-shrink-0 flex flex-col gap-2">
+            <TbDeviceMobileCharging className='h-16 w-16 text-sm p-3 rounded-full bg-white text-cyan-950 mx-2' />
+            <p>RECHARGE</p>
+          </section>
+          <section className="flex-shrink-0 flex flex-col gap-2">
+            <GrCreditCard className='h-16 w-16 text-sm p-3 rounded-full bg-white text-cyan-950 mx-2' />
+            <p>BILL PAYMENT </p>
+          </section>
+          <section className="flex-shrink-0 flex flex-col gap-2">
+            <FaHandHoldingUsd className='h-16 w-16 text-sm p-3 rounded-full bg-white text-cyan-950 mx-2' />
+            <p>LOAN</p>
+          </section>
+          <section className="flex-shrink-0 flex flex-col gap-2">
+            <IoNewspaperOutline className='h-16 w-16 text-sm p-3 rounded-full bg-white text-cyan-950 mx-2' />
+            <p>NEWS</p>
+          </section>
+          <section className="flex-shrink-0 flex flex-col gap-2">
+            <FaPeopleGroup className='h-16 w-16 text-sm p-3 rounded-full bg-white text-cyan-950 mx-2' />
+            <p>JOBS</p>
+          </section>
+          <section className="flex-shrink-0 flex flex-col gap-2">
+            <FaFilePen className='h-16 w-16 text-sm p-3 rounded-full bg-white text-cyan-950 mx-2' />
+            <p>BLOGS</p>
+          </section>
+        </div>
+      </header>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
