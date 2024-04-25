74d702012c8d24c9652d6f1f6f562da888a242a9
SawantAchal <achals1718@gmail.com>
2024-04-26 00:23:43 +0530

    done

--- src/components/MainCarousel.jsx
+++ src/components/MainCarousel.jsx
@@ -1,40 +1,37 @@
-import React from 'react'
-import MC1 from '../assets/MC1.jpg'
-import MC2 from '../assets/MC2.png'
-import MC3 from '../assets/MC3.jpg'
-import MC4 from '../assets/MC4.jpg'
-import MC5 from '../assets/MC5.jpg'
-import MC6 from '../assets/MC6.png'
+import React from 'react';
+import MC1 from '../assets/MC1.jpg';
+import MC3 from '../assets/MC3.jpg';
+import MC4 from '../assets/MC4.jpg';
+import MC5 from '../assets/MC5.jpg';
+import MC6 from '../assets/MC6.png';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import {  Autoplay ,Navigation} from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
+import { Autoplay, Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 const MainCarousel = () => {
-    return (
-    <div className='relative mt-5 bg-white w-full'>
-    <Swiper    loop={true} spaceBetween={0} navigation={true} modules={[Navigation, Autoplay]} autoplay={{   delay: 4500, }} className="h-[50%]">
-      <SwiperSlide>
-        <img src={MC1} alt='' className='w-full'/>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={MC2} alt=''/>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={MC3} alt=''/>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={MC4} alt=''/>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={MC5} alt=''/>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={MC6} alt=''/>
-      </SwiperSlide>
-    </Swiper>
+  return (
+    <div className="relative mt bg-white h-64">
+      <Swiper loop={true} spaceBetween={0} navigation={true} modules={[Navigation, Autoplay]} autoplay={{ delay: 4500 }} className=" h-60 shadow-2xl">
+        <SwiperSlide>
+          <img src={MC1} alt="" className="w-full h-full  object-fill" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={MC3} alt="" className="w-full h-full object-cover" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={MC4} alt="" className="w-full h-full object-cover" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={MC5} alt="" className="w-full h-full object-cover" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={MC6} alt="" className="w-full h-full object-cover" />
+        </SwiperSlide>
+      </Swiper>
     </div>
-  )
-}
+  );
+};
 
-export default MainCarousel
+export default MainCarousel;
+
