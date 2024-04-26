4429e30cd1da44f64a22dd0afd1fd1ee03395281
SawantAchal <achals1718@gmail.com>
2024-04-26 17:09:34 +0530

    json data for contact page

---
+++ src/data/ContactData.js
@@ -0,0 +1,40 @@
+const ContactData = [
+    {
+        id:1,
+        // image:"",
+        name:"Abc def",
+        type:"Grocery"
+    },
+    {
+        id:2,
+        // image:"",
+        name:"def Abc",
+        type:"dairyPrdoct"
+    },
+    {
+        id:3,
+        // image:"",
+        name:"ope def",
+        type:"vegetable"
+    },
+    {
+        id:4,
+        // image:"",
+        name:"hgf def",
+        type:"Grocery"
+    },
+    {
+        id:5,
+        // image:"",
+        name:"bhj def",
+        type:"Grocery"
+    },
+    {
+        id:6,
+        // image:"",
+        name:"mnb def",
+        type:"vegetable"
+    }
+] 
+
+export default ContactData
