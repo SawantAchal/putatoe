b87c2f354d9eab5f13feb970707beb49fd3157b3
SawantAchal <achals1718@gmail.com>
2024-04-26 17:10:22 +0530

    created carousel to display contact by type

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { random } from 'lodash';
 
-const ContactCard = ({ image, name }) => {
+const ContactCard = ({ image, name , type}) => {
   // Function to generate random background color
   const getRandomColor = () => {
     const colors = ['bg-red-400', 'bg-blue-400', 'bg-green-400', 'bg-yellow-400', 'bg-pink-400']; // Add more colors if needed
@@ -14,18 +14,20 @@ const ContactCard = ({ image, name }) => {
 
   return (
     <>
-      <main className='border border-black bg-pink-700 h-80 w-60 rounded-sm'>
-        <div className={`w-64 h-64 rounded-lg overflow-hidden ${hasImage ? '' : getRandomColor()}`}>
+      <main className='border border-black h-80 w-60 rounded-sm'>
+        <div className={`w-60  h-64 rounded-sm overflow-hidden relative ${hasImage ? '' : getRandomColor()}`}>
           {hasImage ? (
             <img src={image} alt={name} className='w-full h-44' />
           ) : (
-            <div className="flex items-center justify-center w-full h-full text-white text-4xl font-bold">
+            <div className="flex  items-center justify-center w-full h-full text-white text-4xl font-bold">
               {name && name.length > 0 ? name.charAt(0) : ''}
             </div>
           )}
+          <div className=' flex inset-0  justify-center items-end  border text-center border-orange-300 absolute'>{name}</div>
         </div>
         <hr />
         <section className='relative'>
+        {/* <div >{name}</div> */}
           <button className='absolute right-11 top-2 bg-slate-500 text-center p-2 w-40 rounded-lg font-bold'>CONTACT</button>
         </section>
       </main>
---
+++ src/components/DairyContactCarousel.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import ContactCard from './ContactCard'
+
+const DairyContactCarousel = ({items}) => {
+  const vegItems = items.filter(item => item.type === 'dairyPrdoct')
+  const responsive = {
+      desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 5,
+        slidesToSlide: 3,
+      },
+      tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 2,
+        slidesToSlide: 1,
+      },
+      mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1,
+        slidesToSlide: 1,
+      },
+    };
+  return (
+    <div className="bg-[#AC8968] shadow-2xl md:py-8 md:p-10 p-0 m-10 rounded-md">
+    <section className='flex justify-between'>
+      <h1 className='font-bold text-xl mb-2 p-3 md:p-0'>Dairy Product Contact </h1>
+      <h1 className='font-bold text-xl mb-2 cursor-pointer p-3 md:p-0'>View All</h1>
+    </section>
+    <Carousel responsive={responsive} autoPlay={true} autoPlaySpeed={2000} infinite={true} arrows={true}>
+      {vegItems.map((item, i) => (
+        <div key={i} className='px-3'>
+          <ContactCard image={item.image} name={item.name} type={item.type} />
+        </div>
+      ))}
+    </Carousel>
+  </div>
+  )
+}
+
+export default DairyContactCarousel
